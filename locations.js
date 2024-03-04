22715946ec69ecb515694bdba1a70c02944c5f75
Harm6049 <145580521+Harm6049@users.noreply.github.com>
2024-03-04 16:31:14 -0500

    javascript file added

---
+++ locations.js
@@ -0,0 +1,95 @@
+//StAuth10244: I Harmeet Singh, 000905920 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else
+const places = [
+    {
+
+        "OBJECTID": 1,
+        "NAME": "Sam Lawrence Park",
+        "ADDRESS": "255 Concession St, Hamilton ON L9A 1B2 ",
+        "TYPE": "Park",
+        "OTHERINFO":"Sam Lawrence Park is a 7.53 hectare (18.6 acre) park situated at the top of the Niagara Escarpment where the Jolley Cut meets Concession Street and Upper Wellington Street. It is best known for its panoramic views of the Lower City, the Dundas Valley, and Lake Ontario",
+        "LATITUDE": 43.24509997535494,
+        "LONGITUDE": -79.86572287481312
+    },
+    {
+       
+        "OBJECTID": 2,
+        "NAME": "William Connell City-Wide Park",
+        "ADDRESS": "1086 West 5th St, Hamilton Ontario L9b 1j6",
+        "TYPE": "Park",
+        "OTHERINFO":"HAMILTON, ON – On Saturday, the City of Hamilton will celebrate the official grand opening of a brand new 20-hectare (49-acre) park located on West 5th between Stone Church Road W and Rymal Road W – the new William Connell City-Wide Park",
+        "LATITUDE": 43.209885605158505,
+        "LONGITUDE": -79.8953050459793
+    },
+    {
+        "OBJECTID": 3,
+        "NAME": "Beasley Park",
+        "ADDRESS": "96 Mary St, Hamilton Ontario L8R 1E1",
+        "TYPE": "Park",
+        "OTHERINFO":"Beasley Park was originally developed in 1976. Until then, Beasley Park was the site of some of Hamilton's early industries, including the Grand Trunk Railway yards.[2] In 1991, the redevelopment of the park was deemed a priority for revitalization of the Beasley neighbourhood.",
+        "LATITUDE": 43.25806292413204,
+        "LONGITUDE": -79.86259473803288
+    },
+    {
+        "OBJECTID": 4,
+        "NAME": "Bayfront Park",
+        "ADDRESS": "200 Harbour Front Drive Bay At Strachan, Hamilton On L8l 1c8",
+        "TYPE": "Park",
+        "OTHERINFO":"A great place to see fireworks and go for a walk or bike ride. Food options at either end of park. Public washrooms in 3 locations. Skate rink open year round as well.",
+        "LATITUDE": 43.271702571660676,
+        "LONGITUDE": -79.87241589015444
+    },
+    {
+        "OBJECTID": 5,
+        "NAME": "Matt Broman Park",
+        "ADDRESS": "Mountain Brow Boulevard 645, Hamilton Ontario L8t 1b2",
+        "TYPE": "Park",
+        "OTHERINFO":"THE NAME: Broman (1895 to 1989) was a prominent landscape architect in Hamilton.",
+        "LATITUDE": 43.21395305525144,
+        "LONGITUDE": -79.8166870036504
+    },
+    {
+        "OBJECTID": 6,
+        "NAME": "Canadian Warplane Heritage Museum",
+        "ADDRESS": "9280 Airport Rd, Mount Hope ON L0R 1W0",
+        "TYPE": "Museum",
+        "OTHERINFO":"The Canadian Warplane Heritage Museum is a Canadian aviation museum located at the John C. Munro Hamilton International Airport in Hamilton, Ontario.",
+        "LATITUDE": 43.16005831951337,
+        "LONGITUDE": -79.92508391899592
+    },
+    {
+        "OBJECTID": 7,
+        "NAME": "Dundurn Castle",
+        "ADDRESS": "610 York Blvd, Hamilton ON L8R 3H1",
+        "TYPE": "Museum",
+        "OTHERINFO":"Experience a guided tour of this 40-room Italianate-style villa built in the 1830's on Burlington Heights; the former site of a fortified military encampment established by the British in 1813.",
+        "LATITUDE": 43.270598811570615,
+        "LONGITUDE": -79.8842684332149
+    },
+    {
+        "OBJECTID": 8,
+        "NAME": "HMCS Haida National Historic Site",
+        "ADDRESS": "658 Catharine St N, Hamilton ON L8L",
+        "TYPE": "Museum",
+        "OTHERINFO":"The last Tribal Destroyer in the world, HMCS HAIDA is Canada’s most famous warship.",
+        "LATITUDE": 43.27551819728334,
+        "LONGITUDE": -79.85545100364715
+    },
+    {
+        "OBJECTID": 9,
+        "NAME": "Hamilton Museum of Steam & Technology",
+        "ADDRESS": "900 woodward Ave, Hamilton, On L8H 7N2",
+        "TYPE": "Museum",
+        "OTHERINFO":"One of the things I like best about Hamilton is the city's pride in its industrial roots. This museum is a case in point.",
+        "LATITUDE": 43.256587542132316,
+        "LONGITUDE":  -79.77202146871828
+    },
+    {
+        "OBJECTID": 10,
+        "NAME": "Art Gallery of Hamilton",
+        "ADDRESS": "123 King St W, Hamilton ON L8P 4S8 ",
+        "TYPE": "Museum",
+        "OTHERINFO":"The Art Gallery of Hamilton is an art museum located in Hamilton, Ontario, Canada. The museum occupies a 7,000 square metres building on King Street West in downtown Hamilton, designed by Trevor P.",
+        "LATITUDE": 43.257589043305764,
+        "LONGITUDE": -79.8721399324838
+    }
+];
